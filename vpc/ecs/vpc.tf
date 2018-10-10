fa770705433263c91209394db01de2cb92829306
James Collerton <jamescollerton@live.co.uk>
2018-10-10 06:33:20 +0100

    Still working on VPC, need to add roles etc

--- vpc/ecs/vpc.tf
+++ vpc/ecs/vpc.tf
@@ -51,6 +51,60 @@ resource "aws_route_table_association" "ecsvpceast1aSN0-0RTAssn" {
   route_table_id = "${aws_route_table.ecsvpcSN0-0RT.id}"
 }
 
+# ECS Instance Security group
+resource "aws_security_group" "ecsvpcsggeneric" {
+    name = "ecsvpcsggeneric"
+    description = "Generic security group for accessing the ECS VPC."
+    vpc_id = "${aws_vpc.ecsvpc.id}"
+
+   ingress {
+       from_port = 22
+       to_port = 22
+       protocol = "tcp"
+       cidr_blocks = [
+          "0.0.0.0/0"]
+   }
+
+   ingress {
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"
+      cidr_blocks = [
+          "0.0.0.0/0"]
+   }
+
+   # Sourcing Data Service Port
+   ingress {
+      from_port = 8082
+      to_port = 8082
+      protocol = "tcp"
+      cidr_blocks = [
+          "0.0.0.0/0"]
+    }
+
+   ingress {
+      from_port = 0
+      to_port = 0
+      protocol = "tcp"
+      cidr_blocks = [
+         "${var.test_public_01_cidr}",
+         "${var.test_public_02_cidr}"]
+    }
+    
+    # Allow all traffic to private SN
+    egress {
+        from_port = "0"
+        to_port = "0"
+        protocol = "-1"
+        cidr_blocks = [
+            "0.0.0.0/0"]
+    }
+
+    tags { 
+       Name = "ecsvpcgeneric"
+     }
+}
+
 terraform {
   backend "s3" {
     bucket  = "artserviceremotestatebucket"
