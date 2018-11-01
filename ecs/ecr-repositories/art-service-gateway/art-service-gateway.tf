cd9fdf3c4e3a8cd3a847651a90de73f1bad4a2e3
James Collerton <jamescollerton@live.co.uk>
2018-11-01 06:44:27 +0000

    Changed repository name

--- ecs/ecr-repositories/art-service-gateway/art-service-gateway.tf
+++ ecs/ecr-repositories/art-service-gateway/art-service-gateway.tf
@@ -1,12 +1,12 @@
 module "art-service-ecr" {
 	source = "../modules"
-	service_name = "art-service-zuul-server"
+	service_name = "art-service-gateway"
 }
 
 terraform {
   backend "s3" {
     bucket  = "art-service-remote-state-bucket"
-    key     = "ecs/ecr-repositories/art-service-zuul-server/terraform.tfstate"
+    key     = "ecs/ecr-repositories/art-service-gateway/terraform.tfstate"
     region  = "us-east-1"
     profile = "s3administrator"
   }
